ff684e276af27a7e8ece5765cc8659fa36b919f1
Elisha Kim <elishakkim@gmail.com>
2020-04-02 00:35:15 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/1ab2e4dfad6589cb4b97946cd5792a23/' + latitude + ',' + longitude +'?units=us'
+    const url = 'https://api.darksky.net/forecast/1ab2e4dfad6589cb4b97946cd5792a23/' + latitude + ',' + longitude +'?units=us&limit=1'
 
     request({ url, json: true}, (error, {body}) => {
         if (error) {
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location! Try another search.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily)
+            callback(undefined, body.daily.data[0].summary + ' Today\'s high is near ' + body.daily.data[0].temperatureHigh + ' and low is around ' + body.daily.data[0].temperatureLow + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
